7c6861b6e620c71ef06a59279e2fb526c9c0ae93
namzikavini <108835068+namzikavini@users.noreply.github.com>
2023-07-08 16:08:30 -0300

    Add files via upload

---
+++ AFSMAXBOOSTS.lua
@@ -0,0 +1,21 @@
+--This script is not work, its a prank
+--by Nockciss TROLAGII
+local v1 = game:GetService("ReplicatedStorage").Multipliers.Damage
+local v2 = game:GetService("ReplicatedStorage").Multipliers.Drops
+local v3 = game:GetService("ReplicatedStorage").Multipliers.Luck
+local v4 = game:GetService("ReplicatedStorage").Multipliers.MeteorShower
+local v5 = game:GetService("ReplicatedStorage").Multipliers.Sale
+local v6 = game:GetService("ReplicatedStorage").Multipliers.Time
+local v7 = game:GetService("ReplicatedStorage").Multipliers.Xp
+local v8 = game:GetService("ReplicatedStorage").Multipliers.Yen
+
+v1.Value = 100
+v2.Value = 100
+v3.Value = 100
+v4.Value = 100
+v5.Value = 100
+v6.Value = 100
+v7.Value = 100
+v8.Value = 100
+
+warn("Script is working!! By @NOCKciss")
